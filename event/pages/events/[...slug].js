af179918f81153c756ea2efcf91a5bfa0d5eafc3
shuvro-baset <md.abdul.baset75@gmail.com>
2022-05-18 19:46:45 +0600

    filter data wip

--- event/pages/events/[...slug].js
+++ event/pages/events/[...slug].js
@@ -1,13 +1,36 @@
 import { useRouter } from 'next/router';
-
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
 
 function FilteredEventsPage(){
     const router = useRouter();
     const filterData = router.query.slug;
     console.log(filterData)
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYears = +filteredYear;
+    const numMonths = +filteredMonth;
+
+    if (isNaN(numYears) || isNaN(numMonths) || numYears > 2030 || numYears < 2021 || numMonths < 1 || numMonths > 12) {
+        return <p>Invalid filter. Please adjust your values!</p>
+    }
+
+    if (!filterData) {
+        return <p className="center">Loading...</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYears,
+        month: numMonths,
+    })
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return <p className="center">No events found for the chosen filter!</p>
+    }
     return(
         <div>
-            <h3>Filtered Events</h3>
+            <EventList items={filteredEvents} />
         </div>
     )
 }
