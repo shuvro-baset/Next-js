0189cf16abc2fb22a1870d6d44f992987d3265e0
shuvro-baset <md.abdul.baset75@gmail.com>
2022-06-08 18:09:18 +0600

    message error done

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-3 bg-white">sidebar</div>
+      <div className="col-span-9 bg-white">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
