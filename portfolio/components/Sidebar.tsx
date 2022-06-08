347b66ebcd4abbf744cb671b410913d419328940
shuvro-baset <md.abdul.baset75@gmail.com>
2022-06-08 22:30:47 +0600

    sidebar wip

---
+++ portfolio/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <img
+        src="https://bestprofilepictures.com/wp-content/uploads/2021/04/Cool-Profile-Picture-986x1024.jpg"
+        alt=""
+      />
+      <h3>
+        <span>Shuvro</span>
+      </h3>
+      <p>Software Engineer</p>
+      <p>Download Resume</p>
+    </div>
+  );
+};
+
+export default Sidebar;
