801f2dc91a36f145595117aa83e40fe84ed5f901
shuvro-baset <md.abdul.baset75@gmail.com>
2022-06-09 23:07:34 +0600

    d

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>bye bye world!</h1>
+        <h1>It's a start of my work station</h1>
       </main>
     </div>
   );
